19daac86e67af326771ce042a91758459db3ae68
mehranHAWAII <mehranh@hawaii.edu>
2022-09-22 22:05:51 -1000

    Completed Part 5 Problem Sets

--- reSwiftPlaygroundTwo.playground/Contents.swift
+++ reSwiftPlaygroundTwo.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 var str = "Hello, playground"
 
-//Part 2
+//Part 2------------------------------------------------------------------
 var goodGrade = "I have been doing all my work"
 if goodGrade == "I am not doing very well"
 {
@@ -12,7 +12,7 @@ if goodGrade == "I am not doing very well"
    print ("I need to work harder in this class")
     
 }
-//Part 4
+//Part 4------------------------------------------------------------------
 var gradeA = true
 if gradeA == true
 {
@@ -20,4 +20,77 @@ if gradeA == true
 }
 else{
     print ("I am not so sure about coding in Swift")
+}
+//---------------------Part 5-------Problem set 1---------------------------
+//Part 5
+//Problem Set 1:
+var yourGrade = 66
+var requirePassingGrade = 70
+if yourGrade < requirePassingGrade {
+    print ("Unfortunalty! You have failed the test!")
+}else{
+    print("Congradulation!, You have passed the test!")
+}
+var yourTestGrade = false
+if yourTestGrade == false{
+    print ("So, you must to retake the test!")
+}else{
+    print("you are qualified to register for the next semester!")
+}
+//----------the bottoms codes are just for my personal exercise------------
+var legalDrivingAge = 18
+var drivingAge = 15
+if drivingAge < legalDrivingAge {
+    print ("you can NOT drive")
+}
+else{
+    print("you can drive")
+}
+
+//Part 5---------------------Exercise--------------------------------------
+//Problem Set 1:
+var intYourGrade = 66
+var intPassingGrade = 70
+if intYourGrade == 66{
+    print("Unfortunalty! You have failed the test!")
+}else{
+     print("Congradulation!, You have passed the test!")}
+var passingGrade = false
+if passingGrade == false {
+    print("So, you must to retake the test")
+}
+else{
+    print("you are qualified to register for the next semester")}
+
+//------------------Part 5---------Problem Set 2---------------------------
+//Part 5
+//Problem set 2:
+var niceWeather = "It is a sunny day!"
+var sunnyDay = "It is a sunny day!"
+if niceWeather == sunnyDay {
+    print("Use sunscreen in a sunny day!")
+}else{
+    print("You don't need sunscreen today!")
+}
+var sunScreen = true
+if sunScreen == true{
+    print("Enjoy your sunshine!")
+}
+var paradise = niceWeather + sunnyDay
+print(paradise)
+
+//-----------------Part 5---------Problem Set 3---------------------------
+var preLoanQualification = "The bank will check your credit score!"
+if preLoanQualification == "The bank will check your credit score!"{
+    print("The crdit score is under 600, Not qualified!")
+}
+else{
+    print("The credit score should be higher!")
+}
+var qualified = false
+if qualified == false {
+    print("You have to rent a place because you are not qualified for a home loan!")
+}
+else{
+    print("The bank will give you a loan!")
 }
