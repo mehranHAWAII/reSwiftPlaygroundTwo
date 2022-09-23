f34ec8f08c12ff7835e09a8c95d8bd8b3222b139
mehranHAWAII <mehranh@hawaii.edu>
2022-09-22 17:19:27 -1000

    Completed Parts 1 - 4

--- reSwiftPlaygroundTwo.playground/Contents.swift
+++ reSwiftPlaygroundTwo.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 2
+var goodGrade = "I have been doing all my work"
+if goodGrade == "I am not doing very well"
+{
+    print("I should getting A")
+}else
+{
+   print ("I need to work harder in this class")
+    
+}
+//Part 4
+var gradeA = true
+if gradeA == true
+{
+    print ("I really like this class")
+}
+else{
+    print ("I am not so sure about coding in Swift")
+}
